598122d9cfd2dd1e8f99aeee450145a0b1dcd18d
dawin <dawin@gmail.com>
2023-01-17 12:20:17 -0300

    titlo lorem

--- new proyect/info.php
+++ new proyect/info.php
@@ -1,4 +1,6 @@
 thurd
 
 
+<h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis aliquam accusamus non iusto id perferendis laborum dolor ducimus saepe pariatur officiis atque maxime, odit, in asperiores sint optio labore nihil.</h1>
+
 
